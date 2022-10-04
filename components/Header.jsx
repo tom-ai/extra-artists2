da5a8df6db2903e874a132816275e76c305aab30
Tom Harris <tomharris3@me.com>
2022-10-04 14:06:58 +0100

    remove unnecessary div blocks

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,12 @@
 const Header = () => {
   return (
-      <div>
-        <h6 className="text-sm font-bold uppercase">Saxophone Edition</h6>
-        <h1 className="mb-3 text-3xl font-bold text-black">Extra Artists</h1>
-        <p className="text-lg text-gray-900">
-          Find the artists that mean something to you!
-        </p>
-      </div>
+    <>
+      <h6 className="text-sm font-bold uppercase">Saxophone Edition</h6>
+      <h1 className="mb-3 text-3xl font-bold text-black">Extra Artists</h1>
+      <p className="text-lg text-gray-900">
+        Find the artists that mean something to you!
+      </p>
+    </>
   );
 };
 
