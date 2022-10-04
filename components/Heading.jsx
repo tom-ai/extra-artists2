f4b5b7475f87b26587554263b4b7ab6d195b890f
Tom Harris <tomharris3@me.com>
2022-10-04 12:03:24 +0100

    add link to discogs

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,8 +1,16 @@
+import Link from 'next';
+
 const Heading = () => {
   return (
     <>
       <h2 className="text-2xl font-bold">1. Find a Song</h2>
-      <p className="text-lg">Using Discogs, copy the ID</p>
+      <p className="text-lg">Find a song on Discogs and copy the ID</p>
+      <a
+        className="text-lg font-bold underline"
+        href="https://www.discogs.com/"
+      >
+        https://www.discogs.com/
+      </a>
     </>
   );
 };
