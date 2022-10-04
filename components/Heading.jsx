0b9e635c8d1b6c5d6eb027e8beb92a244226b9cf
Tom Harris <tomharris3@me.com>
2022-10-04 12:04:21 +0100

    change title

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -3,7 +3,7 @@ import Link from 'next';
 const Heading = () => {
   return (
     <>
-      <h2 className="text-2xl font-bold">1. Find a Song</h2>
+      <h2 className="text-2xl font-bold">1. Get Song ID</h2>
       <p className="text-lg">Find a song on Discogs and copy the ID</p>
       <a
         className="text-lg font-bold underline"
