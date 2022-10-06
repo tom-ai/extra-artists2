11a383c94d6252a2d52a1d5ccd7b54e2fb7d3d76
Tom Harris <tomharris3@me.com>
2022-10-06 17:32:45 +0100

    tiday up function

--- utils/artistIsOnSongObject.js
+++ utils/artistIsOnSongObject.js
@@ -1,23 +1,11 @@
 // given array of top level extra artists, function returns true if saxophone is found
-
-const artistIsOnSongObject = (array, instrument) => {
-  // search array for role
-  // console.log('triggered!', array);
-
-  const regex = /sax/i;
-  array.forEach((artist) => {
-    // console.log(artist.role, regex.test(artist.role));
-    if (regex.test(artist.role)) {
-      console.log('yes sax', artist.name);
-    }
-  });
-
-  // if role contains "saxophone"
-  // return true
-};
-
 // then when mapping out, the saxophone is in top level so map out top level
 
-// otherwise extra artist info is nested in within 'tracklist' array
+const artistIsOnSongObject = (array, instrument) => {
+  const regex = `/${instrument}/i`;
+  array.forEach((artist) => {
+    regex.test(artist.role) ? true : false;
+  });
+};
 
 export default artistIsOnSongObject;
